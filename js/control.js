41b1c2e64c8244f6847262b08b3d9c8e354464c2
great <danilkazakov2007@gmail.com>
2021-04-20 01:47:07 +0300

    Add dark theme

--- js/control.js
+++ js/control.js
@@ -22,6 +22,72 @@ var arrLang = {
         'first_name_N' : 'Никита Тимошенко',
     }
 };
+//var btn = jQuery(".theme-button");
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+let lightThemeMain = "main_light.css";
+let darkThemeMain = "main_dark.css";
+let lightThemeSubjects = "../../../subjects/subject_page_light.css";
+let darkThemeSubjects = "../../../subjects/subject_page_dark.css";
+
+getTheme();
+function ChangeTheme()
+{
+
+    myStorage = window.localStorage;
+    var currTheme = link.getAttribute("href");
+    if(currTheme == lightThemeMain)
+    {
+        currTheme = darkThemeMain;
+        localStorage.setItem('Theme', 'dark');
+    }
+    else if (currTheme == darkThemeMain)
+    {
+        currTheme = lightThemeMain;
+        localStorage.setItem('Theme', 'light');
+    }
+    else if(currTheme == lightThemeSubjects)
+    {
+        currTheme = darkThemeSubjects;
+        localStorage.setItem('Theme', 'dark');
+    }
+    else if (currTheme == darkThemeSubjects)
+    {
+        currTheme = lightThemeSubjects;
+        localStorage.setItem('Theme', 'light');
+    };
+    console.log(localStorage.getItem('Theme'));
+    link.setAttribute("href", currTheme);
+};
+
+function getTheme()
+{
+    var currTheme = link.getAttribute("href");
+    myStorage = window.localStorage;
+    var theme = localStorage.getItem('Theme');
+    console.log(theme);
+    if (theme == 'dark') {
+        if (currTheme == lightThemeMain)
+        {
+            $(function() {
+                document.getElementById("theme-button").checked = true;
+            });
+            currTheme = darkThemeMain;
+            localStorage.setItem('Theme', 'dark');
+        }
+        else if (currTheme == lightThemeSubjects)
+        {
+            $(function() {
+                document.getElementById("theme-button").checked = true;
+            });
+            currTheme = darkThemeSubjects;
+            localStorage.setItem('Theme', 'dark');
+        };
+    };
+    link.setAttribute("href", currTheme);
+};
+
+/*
 $(function() {
     var lang = getLanguage()
     console.log(lang)
@@ -55,4 +121,8 @@ function getLanguage() {
     myStorage = window.localStorage;
     var lang = localStorage.getItem('Language');
     return lang;
-}
+}*/
+
+
+
+
